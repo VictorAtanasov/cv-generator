325f03d96d8faf1c38d12ca6c113610db41e6263
Victor Atanasov <viktor_atanasovv@yahoo.com>
2017-12-08 17:49:00 +0200

    react-input-autosize package added to the cv technologies component; CvTechnologiesTextarea changed to class component from stateless component; cv inner pop over added to the technologies component

--- src/components/CVcomponents/CvTechnologiesComponent.js
+++ src/components/CVcomponents/CvTechnologiesComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import FontAwesomeCvPage from '../FontAwesomeCvPage';
 import CvTechnologiesTextarea from '../forms/CvTechnologiesTextarea';
 import CvTextarea from '../forms/CvTextarea';
+import Paper from 'material-ui/Paper';
 import _ from 'lodash';
 
 export default class CvTechnologiesComponent extends React.Component{
@@ -13,34 +14,12 @@ export default class CvTechnologiesComponent extends React.Component{
         }
 
         this.renderTechnologies = this.renderTechnologies.bind(this);
-        this.pushTechnologyData = this.pushTechnologyData.bind(this);
-        this.setTechnologyData = this.setTechnologyData.bind(this);
         this.deleteComponent = this.deleteComponent.bind(this);
         this.addNewComp = this.addNewComp.bind(this);
         this.hideOptions = this.hideOptions.bind(this);
         this.showOptions = this.showOptions.bind(this);
         this.addTechnology = this.addTechnology.bind(this);
         this.pushData = this.pushData.bind(this);
-    }
-
-    pushTechnologyData(e){
-        let userUid = this.props.userInfo.userUid;
-        let data = {
-            technology: 'technology'
-        }
-        let targetValue = {
-            'technology': e.target.value
-        }
-        let componentId = this.props.id;
-        let key = e.target.id;
-        if(e.key === 'Enter'){
-            this.props.pushMultipleComponentData(userUid, this.props.type, componentId, 'technologies', data);
-            this.props.setMultipleComponentNestedData(userUid, this.props.type, componentId, 'technologies', key, targetValue);
-        } else if (e.key === 'Backspace'){
-            if(e.target.value === ''){
-                this.props.deleteMultipleComponentNestedData(userUid, this.props.type, componentId, 'technologies', key, targetValue);
-            }
-        }
     }
 
     addTechnology(){
@@ -50,27 +29,14 @@ export default class CvTechnologiesComponent extends React.Component{
         this.props.pushMultipleComponentData(this.props.userInfo.userUid, this.props.type, this.props.id, 'technologies', data);
     }
 
-    setTechnologyData(e){
-        
-        let userUid = this.props.userInfo.userUid;
-        console.log(e.target.value)
-        // let targetValue = {
-        //     'technology': e.target.value
-        // }
-        // let componentId = this.props.id;
-        // let key = e.target.id;
-        // this.props.setMultipleComponentNestedData(userUid, this.props.type, componentId, 'technologies', key, targetValue);
-    }
-
     renderTechnologies(){
         return _.map(this.props.cv.cvData[this.props.type][this.props.id].technologies, (technology, key) => {
             return <CvTechnologiesTextarea 
-                        type="text"
                         name={technology.technology}
                         placeholder="Your Technology"
-                        onBlur={this.setTechnologyData}
-                        onKeyDown={this.pushTechnologyData}
                         className="cv-technologies-input"
+                        compId={this.props.id}
+                        {...this.props}
                         id={key}
                         key={key}
                     />
@@ -119,13 +85,29 @@ export default class CvTechnologiesComponent extends React.Component{
 
     render(){
         return(
-            <div className="experienceWarpper" onMouseEnter={this.showOptions} onMouseLeave={this.hideOptions}>
-                <div className={this.state.buttonClass}>
-                    <button onClick={this.deleteComponent}><FontAwesomeCvPage font="trash" /></button>
-                    <button onClick={this.addNewComp}><FontAwesomeCvPage font="plus" /></button>
-                    <button onClick={this.addTechnology}><FontAwesomeCvPage font="plus-circle" /></button>
-                </div>
-                <div>
+
+            <div 
+                className="experienceWarpper" 
+                onMouseEnter={this.showOptions} 
+                onMouseLeave={this.hideOptions}
+            >
+                    <Paper 
+                        zDepth={1}
+                        className={this.state.buttonClass + ' ' + 'componentOptionsPopOver'}
+                    >
+                        <span onClick={this.deleteComponent}>
+                            <FontAwesomeCvPage font="trash" />
+                        </span>
+                        <span onClick={this.addNewComp}>
+                            <FontAwesomeCvPage font="plus" />
+                        </span>
+                        <span onClick={this.showInnerPopOverOptions}>
+                            <FontAwesomeCvPage font='cog' />
+                        </span>
+                        <span onClick={this.addTechnology}>
+                            <FontAwesomeCvPage font="plus-circle" />
+                        </span>
+                    </Paper>
                     <CvTextarea 
                         type="text"
                         name={this.props.cv.cvData[this.props.type][this.props.id]['title']['group-title']}
@@ -134,7 +116,6 @@ export default class CvTechnologiesComponent extends React.Component{
                         className="cv-standart-blue textarea-default"
                         id="group-title"
                     />
-                </div>
                 <div>
                     {this.renderTechnologies()}
                 </div>
--- src/components/forms/CvTechnologiesTextarea.js
+++ src/components/forms/CvTechnologiesTextarea.js
@@ -1,20 +1,68 @@
 import React from 'react';
+import AutosizeInput from 'react-input-autosize';
 
-const CvTechnologiesTextarea = (props) => {
 
-    return(
-            <div className="cv-texhnologies-wrapper">
-                <input 
-                    className={props.className} 
-                    placeholder={props.placeholder}
-                    id={props.id} 
-                    onBlur={props.onBlur}
-                    //onKeyDown={props.onKeyDown}
-                    defaultValue={props.name}
-                    spellCheck="false"
-                /> 
-            </div>
-    )
+export default class CvTechnologiesTextarea extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state={
+            html: this.props.name
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.setTechnologyData = this.setTechnologyData.bind(this);
+        this.pushTechnologyData = this.pushTechnologyData.bind(this);
+    }
+
+    handleChange(e){
+        this.setState({
+            html: e.target.value
+        })
+    }
+
+
+    setTechnologyData(e){
+        let userUid = this.props.userInfo.userUid;
+        let targetValue = {
+            'technology': this.state.html
+        }
+        let componentId = this.props.compId;
+        let key = e.target.id;
+        this.props.setMultipleComponentNestedData(userUid, this.props.type, componentId, 'technologies', key, targetValue);
+    }
+    
+    pushTechnologyData(e){
+        let userUid = this.props.userInfo.userUid;
+        let data = {
+            technology: 'technology'
+        }
+        let targetValue = {
+            'technology': e.target.value
+        }
+        let componentId = this.props.compId;
+        let key = e.target.id;
+        if(e.key === 'Enter'){
+            this.props.pushMultipleComponentData(userUid, this.props.type, componentId, 'technologies', data);
+            this.props.setMultipleComponentNestedData(userUid, this.props.type, componentId, 'technologies', key, targetValue);
+        } else if (e.key === 'Backspace'){
+            if(e.target.value === ''){
+                this.props.deleteMultipleComponentNestedData(userUid, this.props.type, componentId, 'technologies', key, targetValue);
+            }
+        }
+    }
+
+    render(){
+        return(
+            <AutosizeInput
+                onBlur={this.setTechnologyData}
+                value={this.state.html}
+                placeholder={this.props.placeholder}
+                id={this.props.id}
+                onChange={this.handleChange}
+                onKeyDown={this.pushTechnologyData}
+                spellCheck="false"
+            />
+        )
+    }
 }
-
-export default CvTechnologiesTextarea;
